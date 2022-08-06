1f66641007a740a9738f49cb82ee560073588a2b
tulasikumar17 <96835369+tulasikumar17@users.noreply.github.com>
2022-08-06 14:24:16 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -19,8 +19,9 @@ let p=50;
 let track=document.createElement('audio');
 let t=0;
 let isRandom=false;
+let rep_val="repeat";
 
-let naa_song=[
+let All_song=[
     {Name: "Arabic kuthu", Path: "songs/1.mp3" ,imgPath: "covers/1.jpg"},
     {Name: "Catch Me", Path: "songs/2.mp3",imgPath: "covers/2.jpg"},
     {Name: "Attasudake", Path: "songs/3.mp3",imgPath: "covers/3.jpg"},
@@ -30,10 +31,29 @@ let naa_song=[
     {Name: "jorsey", Path: "songs/7.mp3",imgPath: "covers/5.jpg"},
     {Name: "Bangaaru kodi", Path: "songs/8.mp3",imgPath: "covers/5.jpg"},
     {Name: "Nakosam nuvvu", Path: "songs/9.mp3",imgPath: "covers/5.jpg"},
-    {Name: "Rolling title", Path: "songs/10.mp3",imgPath: "covers/5.jpg"}
+    {Name: "Rolling title", Path: "songs/10.mp3",imgPath: "covers/5.jpg"},
+    {Name: "lala", Path: "songs/11.mp3" ,imgPath: "covers/1.jpg"},
+    {Name: "Ga ga megha", Path: "songs/12.mp3",imgPath: "covers/2.jpg"},
+    {Name: "Seheri", Path: "songs/13.mp3",imgPath: "covers/3.jpg"},
+    {Name: "Idhi chaala bagundi", Path: "songs/14.mp3",imgPath: "covers/4.jpg"},
+    {Name: "Vaaram", Path: "songs/15.mp3",imgPath: "covers/5.jpg"},
+    {Name: "Beast mode", Path: "songs/16.mp3",imgPath: "covers/5.jpg"},
+    {Name: "Bonalu", Path: "songs/17.mp3",imgPath: "covers/5.jpg"},
+    {Name: "Darshana", Path: "songs/18.mp3",imgPath: "covers/5.jpg"},
+    {Name: "Hara hara", Path: "songs/19.mp3",imgPath: "covers/5.jpg"},
+    {Name: "Tujh Mein Rab Diktha", Path: "songs/20.mp3",imgPath: "covers/5.jpg"},
+    {Name: "Vikram title", Path: "songs/21.mp3",imgPath: "covers/3.jpg"},
+    {Name: "suthana", Path: "songs/22.mp3",imgPath: "covers/5.jpg"},
+    {Name: "Toofan", Path: "songs/23.mp3",imgPath: "covers/5.jpg"},
+    {Name: "Yadagara", Path: "songs/24.mp3",imgPath: "covers/2.jpg"},
+    {Name: "Mehebooba", Path: "songs/25.mp3",imgPath: "covers/5.jpg"},
+    {Name: "The monster", Path: "songs/26.mp3",imgPath: "covers/5.jpg"},
+    {Name: "Brindavanam", Path: "songs/27.mp3",imgPath: "covers/1.jpg"},
+    {Name: "Yaad piya ki aane lagi", Path: "songs/28.mp3",imgPath: "covers/1.jpg"},
+    {Name: "manike mage hithe", Path: "songs/29.mp3",imgPath: "covers/4.jpg"}
 ];
-let All_song=[
-    {Name: "la la", Path: "songs/11.mp3" ,imgPath: "covers/11.jpg"}
+let naa_song=[
+    {Name: "la la", Path: "songs/11.mp3" ,imgPath: "covers/1.jpg"}
 ]
 //load track
 function load_track(index_no){
@@ -56,6 +76,9 @@ function justplay(){
     }
 }
 
+function random_song(){
+    index_no=Math.floor(Math.random()*(All_song.length))
+}
 function playsong(){
     track.play();
     playing_song=true;
@@ -70,13 +93,21 @@ function pausesong(){
 //song navigations
 function previous_song(){
     if(index_no<=0){
+        if(rep_val=="shuffle"){
+            random_song()
+        }else{
         index_no=All_song.length-1;
+        }
         present.innerText=""+(index_no+1)+"";
         load_track(index_no);
         playsong();
     }
     else{
+        if(rep_val=="shuffle"){
+            random_song()
+        }else{
         index_no-=1;
+        }
         present.innerText=""+(index_no+1)+"";
         load_track(index_no);
         playsong();
@@ -85,13 +116,21 @@ function previous_song(){
 
 function next_song(){
     if(index_no>=(All_song.length-1)){
+        if(rep_val=="shuffle"){
+            random_song()
+        }else{
         index_no=0;
+        }
         present.innerText=""+(index_no+1)+"";
         load_track(index_no);
         playsong();
     }
     else{
+        if(rep_val=="shuffle"){
+            random_song()
+        }else{
         index_no+=1;
+        }
         present.innerText=""+(index_no+1)+"";
         load_track(index_no);
         playsong();
@@ -107,6 +146,17 @@ function volume_change(){
 function change_duration(){
     slider_position=(track.duration*slider.value)/100;
     track.currentTime=slider_position;
+}
+//volume derease on key value
+function volume_dec(){
+    p=recent_volume.value;
+    recent_volume.value=p-5;
+    volume_show.innerText=recent_volume.value;
+    track.volume=recent_volume.value;
+}
+//volume increase on key value
+function volume_inc(){
+    //o.....
 }
 //duration update on slider bar
 track.addEventListener("timeupdate",()=>{
@@ -126,13 +176,7 @@ track.addEventListener("timeupdate",()=>{
         document.querySelector("#show_duration").innerText="0"+Math.floor(q/60)+":"+Math.floor(q%60);
     }
     if(track.currentTime==track.duration){
-        if(rep.innerText=="repeat"){
-            track.currentTime=0;
-            track.play();
-        }
-        else{
-            next_song();
-        }
+        next_song()
     }
 })
 //mute or unmute
@@ -150,19 +194,54 @@ document.getElementById("volume_icon").addEventListener("click",()=>{
         track.volume=recent_volume.value/100;
         document.getElementById("volume_icon").src="vol_inc.webp";
     }
-})
-
-document.onkeydown=function(e){
-    if(e.keycode==32 || e.keycode==107){
-        justplay();
+})    
+function mute(){
+    if(recent_volume.value!=0){
+        p=recent_volume.value;
+        recent_volume.value=0;
+        volume_show.innerText=recent_volume.value;
+        track.volume=recent_volume.value/100;
+        document.getElementById("volume_icon").src="mut.webp";
     }
-    else if(e.keycode==108) {
-        next_song();
-    }
-    else if(e.keycode==106){
-        previous_song();
+    else{
+        recent_volume.value=p;
+        volume_show.innerText=recent_volume.value;
+        track.volume=recent_volume.value/100;
+        document.getElementById("volume_icon").src="vol_inc.webp";
     }
 }
+
+document.addEventListener('keyup',function(e){
+    if(e.code=='ArrowRight'){
+        next_song();
+        console.log("tul")
+    }
+    if(e.code=='ArrowLeft'){
+        previous_song();
+        
+    }
+    if(e.code=='Space'){
+        justplay();
+    }
+    if(e.code=='KeyS'){
+        shuffle();
+    }
+    if(e.code=='ArrowDown'){
+        volume_dec();
+    }
+    if(e.code=='ArrowUp'){
+        volume_inc();
+    }
+    if(e.code=='KeyJ'){
+        backward();
+    }
+    if(e.code=='KeyL'){
+        forward();
+    }
+    if(e.code=='KeyM'){
+        mute();
+    }
+});
 
 function forward(){
     if(t==0){
@@ -172,10 +251,16 @@ function forward(){
 }
 
 function backward(){
-    if(rep.innerText=="repeat"){
-        rep.innerText="rep";
-    }else{
-        rep.innerText="repeat"
-    }
     track.currentTime-=10;
+}
+
+function shuffle(){
+    if(rep_val=="repeat"){
+        rep_val="shuffle";
+        rep.src="shuffle_but.png";
+    }else{
+        rep_val="repeat";
+        rep.src="repeat_but.png"
+    }
+    
 }
